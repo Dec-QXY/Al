19abe195aa87b1a15888efbb1fd30ef9a9ccc520
decqxy <202406865@qq.com>
2021-01-10 22:43:19 +0800

    updata StudyStack.java

--- src/com/company/chapter3/StudyStack.java
+++ src/com/company/chapter3/StudyStack.java
@@ -5,24 +5,53 @@ package com.company.chapter3;
  */
 public class StudyStack {
 
-    public static void main(String[] args) {
-
-    }
-}
-
-class StackJava {
-    /**
-     * 定义栈的大小
-     */
     private int maxSize;
-
-    private int[] data;
-
+    private int[] stackArray;
     private int top;
 
+    public StudyStack(int s) {
+        // 初始化栈
+        maxSize = s;
+        stackArray = new int[maxSize];
+        top = -1;
+    }
 
+    public void push(int j) {
+        if (isFull()) {
+            System.out.println("isFull " + j);
+            return;
+        }
+        stackArray[++top] = j;
+    }
 
-    public boolean ifFull() {
-        return top == maxSize - 1;
+    public long pop() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return stackArray[top--];
+    }
+
+    public long peek() {
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+
+    public static void main(String[] args) {
+        StudyStack stack = new StudyStack(10);
+        for (int i = 1; i <= stack.maxSize; i++) {
+            stack.push(i);
+        }
+        System.out.println("输出的栈顺序为：");
+        while (!stack.isEmpty()) {
+            long value = stack.pop();
+            System.out.print(value);
+            System.out.print(" ");
+        }
     }
 }
